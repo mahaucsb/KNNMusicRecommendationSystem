c5c10b6fbd92bd67b4d1643db44f8f8c69df138f
bo <ninj0x@xubuntu.(none)>
2012-10-24 15:26:40 -0700

    improved run.sh and build file

--- KDDKNN-exact/run.sh
+++ KDDKNN-exact/run.sh
@@ -1,31 +1,59 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #############################################
-#You have to put your data initially in the
-#location specified by $dataDir of this  
-#script.For query, you need to put it in the
-#location shown in $queryFile. Then just 
-#run the script.
+# Usage: run.sh [clean]
 #
-#To see output:
-#Since results are stored in hdfs in sequence
-#format, you need to run the following to 
-#read them as:
-#hjar target/kddknn.jar readseq <part-0000>
+# Run by itself, the script will populate the
+# HDF with a dataset and process it. Then it
+# will ask a song to query and retunr the
+# result.
+#
+# clean: remove all files from the HDF made
+# with this script.
+#
+# You have to put your data initially in the
+# location specified by $dataDir of this
+# script.For query, you need to put it in the
+# location shown in $queryFile. Then just
+# run the script.
+#
+# To see output:
+# Since results are stored in hdfs in sequence
+# format, you need to run the following to
+# read them as:
+# hjar target/kddknn.jar readseq <part-0000>
 #############################################
 
 dataDir=../data/test
 queryFile=../data/query/u1
-
-HADOOP=/Users/Hadoop/hadoop-0.20.2/bin/hadoop
+JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386/jre
+HADOOP=/home/ninj0x/Downloads/hadoop-0.20.2/bin/hadoop
 xmlconf=src/main/resources/kddknn/conf.xml
 knnjar=target/kddknn.jar
 
-$HADOOP dfs -rmr knn-input
-$HADOOP dfs -put $dataDir knn-input
-#$HADOOP jar $knnjar invindex -conf $xmlconf 
-#$HADOOP jar $knnjar invertedknn -conf $xmlconf 
+clean() {
+	$HADOOP dfs -rmr sinvertedindex
+	$HADOOP dfs -rmr knn-output
+	$HADOOP dfs -rmr knn-input
+	$HADOOP dfs -rmr query
+}
 
-$HADOOP dfs -rmr query
+if [[ -n $JAVA_HOME ]]; then
+	export JAVA_HOME
+fi
+
+if [[ $1 = "clean" ]]; then
+	clean
+	ant clean
+	exit
+fi
+
+# Build from source.
+ant build
+clean
+$HADOOP dfs -put $dataDir knn-input
+$HADOOP jar $knnjar invindex -conf $xmlconf
+$HADOOP jar $knnjar invertedknn -conf $xmlconf
+
 $HADOOP dfs -put $queryFile query
-$HADOOP jar $knnjar queryknn -conf $xmlconf 
+$HADOOP jar $knnjar queryknn -conf $xmlconf
